97454e877c9568ee709fae9f0a34fce5d257cea2
Lykosis2 <154882230+Lykosis2@users.noreply.github.com>
2025-08-17 18:00:38 +0300

    Delete pages/api directory

--- pages/api/test/pos.js
+++
@@ -1,8 +0,0 @@
-import akpos3dsInitialize from "../../../lib/akpos3dsInitialize";
-import informTheAdmin from "../../../lib/errorHandle/informTheAdmin";
-
-export default async function handler(req,res){
-    await akpos3dsInitialize(req.body.orderId,req.body.amount).then((data)=>{
-        res.status(200).json(data)
-    })
-}
