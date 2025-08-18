c846fa478eeb063a913cbe5b684e5fec3c681907
Lykosis2 <154882230+Lykosis2@users.noreply.github.com>
2025-08-18 08:19:16 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,3 @@
-import React, { useEffect } from 'react'
-import { useRouter } from 'next/router'
-import { NavbarTextContext } from '@/components/NavbarProvider'
-
 export default function Home() {
   return (
     <main style={{padding:"2rem", fontFamily:"sans-serif"}}>
@@ -9,27 +5,4 @@ export default function Home() {
       <p>Bu sayfa Next.js ile çalışıyor.</p>
     </main>
   );
-}
-
-export default function Home() {
-  const { navbarText, filter, showProducts } = React.useContext(NavbarTextContext)
-  const router = useRouter()
-
-  useEffect(() => {
-    return () => {
-      router.asPath = `/home?timestamp=${Date.now()}`
-    }
-  }, [])
-
-  useEffect(() => {
-    const shouldRedirect = navbarText.length > 0 || filter !== 0 || showProducts
-
-    router.push(shouldRedirect ? '/filter' : '/home')
-  }, [navbarText, filter, showProducts])
-
-  return (
-    <>
-
-    </>
-  )
 }
