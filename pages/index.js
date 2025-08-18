cc9dd790c4f7892ce8d408acdb41ecadd9f4b845
Lykosis2 <154882230+Lykosis2@users.noreply.github.com>
2025-08-18 08:07:36 +0300

    Delete pages/index.js

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{padding:"24px", fontFamily:"sans-serif"}}>
-      <h1>LYKOSis — Çalışıyor 🎉</h1>
-      <p>Next.js ana sayfa testi.</p>
-    </main>
-  );
-}
